40d7dc1ee5da0f44a4915ebc3684719d5bcfcd46
vampyde <blackyde@gmail.com>
2019-06-05 16:27:01 +0900

    테스트

--- 0531_Ledger/test/kr/ac/kopo/ctc/kopo08/dao/Developer.java
+++ 0531_Ledger/test/kr/ac/kopo/ctc/kopo08/dao/Developer.java
@@ -4,27 +4,27 @@ package kr.ac.kopo.ctc.kopo08.dao;
 //import java.sql.PreparedStatement;
 //import java.sql.ResultSet;
 //import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
+import java.util.Scanner;
 
 //import kr.ac.kopo.ctc.kopo08.conn.Conn;
-import kr.ac.kopo.ctc.kopo08.domain.AccountItem;
+//import kr.ac.kopo.ctc.kopo08.domain.AccountItem;
 
 public class Developer {
     
-    public String name;
-    public int career;
-     
     public static void main(String[] args){
+    	
+    	Scanner sc = new Scanner(System.in);
      
 //        for(Category type : Category.values()){
 //            System.out.println(type.name() + " " + type.getName());
 //        }
     	
-    	List<String> list = new ArrayList<String>();
+//    	List<String> list = new ArrayList<String>();
 //    	List<String> list2 = new ArrayList<String>();
-    	list.add("id");
-    	list.add("1");
+//    	list.add("id");
+//    	list.add("1");
     	
     	
     			
@@ -44,17 +44,31 @@ public class Developer {
 //			e.printStackTrace();
 //		}
     	
-		AccountItemDao dao = new AccountItemDaoImpl();
-		List<String> filter = new ArrayList<String>();
-		filter.add("id");
-		filter.add("1");
-		filter.add("3");
-		//String sql = "SELECT * FROM AccountItem WHERE " + filter.get(0) + " BETWEEN ? AND ?";
-		List<AccountItem> alist = dao.selectContainsTitle(filter);
+//		AccountItemDao dao = new AccountItemDaoImpl();
+//		List<String> filter = new ArrayList<String>();
+//		filter.add("id");
+//		filter.add("1");
+//		filter.add("3");
+//		//String sql = "SELECT * FROM AccountItem WHERE " + filter.get(0) + " BETWEEN ? AND ?";
+//		List<AccountItem> alist = dao.selectContainsTitle(filter);
+//		
+//		for(AccountItem aItem : alist) {
+//			System.out.println(aItem.getId() + " " + aItem.getTitle());
+//		}
 		
-		for(AccountItem aItem : alist) {
-			System.out.println(aItem.getId() + " " + aItem.getTitle());
+		String str = "password";
+		System.out.println(str.hashCode());
+		
+		System.out.print("비밀번호 : ");
+		String password = sc.nextLine();
+		
+		if(password.hashCode() == 1216985755) {
+			System.out.println("성공");
+		} else {
+			System.out.println("실패");
 		}
+		
+		sc.close();
     	
     }
  
